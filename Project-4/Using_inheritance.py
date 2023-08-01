befc9f39ac5b6ebe5d580d86bd28e007c133ce58
Code-To-Future <111292765+Code-To-Future@users.noreply.github.com>
2023-08-01 20:44:24 +0530

    Update Using_inheritance.py
    Implementing multilevel inheritance.

--- Project-4/Using_inheritance.py
+++ Project-4/Using_inheritance.py
@@ -1,6 +1,30 @@
 #Program: Implementing concepts of inheritance
-#This is single level inheritance:
+#This is Multilevel inheritance:
 class Subparticles:
     pass
 class Atoms(Subparticles):
     pass
+class Molecules(Atoms):
+    pass
+class Allotropes(Molecules):
+    pass
+
+#Objects of class Subparticles:
+electron= Subparticles()
+proton= Subparticles()
+neutron= Subparticles()
+
+#Objects of class Atoms:
+Oxygen= Atoms()
+Nitrogen= Atoms()
+Carbon= Atoms()
+
+#Objects of class Molecules:
+O3= Molecules()
+N2= Molecules()
+C60= Molecules()
+
+#Objects of class Allotropes:
+Ozone= Allotropes()
+NitrogenGas= Allotropes()
+BuckminsterFullerene= Allotropes()
