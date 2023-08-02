b8d73c6773ecc468e9b1d7b0f98dac870c79f6e4
Code-To-Future <111292765+Code-To-Future@users.noreply.github.com>
2023-08-03 02:15:08 +0530

    Update gtngdocinfo.py
    Now, getting access of multiple pdfs info is possible.

--- Project-5 PDF Handling using Python/gtngdocinfo.py
+++ Project-5 PDF Handling using Python/gtngdocinfo.py
@@ -1,9 +1,12 @@
 #Python program to get PDF info using PyPDF2 Library
 from PyPDF2 import PdfFileReader
 
-pdf_name="D:\COMICS\NINJAS\PDFs\Antony Cummins' and Yoshie Minami's 'The Secret Traditions of the Shinobi (Hattori Hanzo's Shinobi Hiden and Other Ninja Scrolls)'.pdf"
-f = open(pdf_name,'rb')
-pdf = PdfFileReader(f)
-information = pdf.getDocumentInfo()
-print(information)
+var="Y"
+while(var=="y" or var=="Y"):
+  pdf_name=input("Enter file name along with its path: ")
+  f = open(pdf_name,'rb')
+  pdf = PdfFileReader(f)
+  information = pdf.getDocumentInfo()
+  print(information)
+  var= input("Press Y or y to get info of another pdf or press any other key to quit.")
 f.close()
